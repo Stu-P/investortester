ba2956271a40a3f92863ad11e9fab32f63c50686
Stuart Parton <stuartparton@gmail.com>
2020-07-05 17:13:08 +1000

    add throws ex to method

--- src/main/java/codetest/stuart/investortester/services/InvestmentReturnCalculator.java
+++ src/main/java/codetest/stuart/investortester/services/InvestmentReturnCalculator.java
@@ -2,7 +2,7 @@ package codetest.stuart.investortester.services;
 
 public class InvestmentReturnCalculator {
 
-    public int getMaxTradeProfit(int[] prices) {
+    public int getMaxTradeProfit(int[] prices) throws IllegalArgumentException {
 
         if (prices == null || prices.length < 2) {
             throw new IllegalArgumentException("input must contain at least 2 prices");
@@ -13,14 +13,14 @@ public class InvestmentReturnCalculator {
         int bestTradeProfit = sellPriceCandidate - buyPriceCandidate;
 
         for (int i = 1; i < prices.length-1; i++) {
-            var nextBuyPrice = prices[i];
+            int nextBuyPrice = prices[i];
             sellPriceCandidate = prices[i + 1];
 
             if (nextBuyPrice < buyPriceCandidate)
             {
                 buyPriceCandidate = nextBuyPrice;
             }
-            var tradeProfit = sellPriceCandidate - buyPriceCandidate;
+            int tradeProfit = sellPriceCandidate - buyPriceCandidate;
             if( tradeProfit > bestTradeProfit)
             {
                 bestTradeProfit = tradeProfit;
